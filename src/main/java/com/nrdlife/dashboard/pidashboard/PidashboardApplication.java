87bd12821db637801657a50dfae4d64755000017
Nicholas Dorans <Nicky_Bobby@icloud.com>
2019-01-05 13:09:13 -0600

    initial commit

---
+++ src/main/java/com/nrdlife/dashboard/pidashboard/PidashboardApplication.java
@@ -0,0 +1,14 @@
+package com.nrdlife.dashboard.pidashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PidashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PidashboardApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/nrdlife/dashboard/pidashboard/PidashboardApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nrdlife.dashboard.pidashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PidashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
